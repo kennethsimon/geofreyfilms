0da062492d7e7c97d7eb209f1aff1e22d7ca422f
kennethsimon <kenethsimon@gmail.com>
2018-12-14 20:05:27 +0300

    added animation on the aboutwords area
    animation

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 $(document).ready(function(){
     $('.parallax').parallax();
     $('.materialboxed').materialbox();
+    $('.about-word-area').hide();
     $('.link-area-header').css('display', 'none');
     $('.link-area-header a').click(function(){
         $('.link-area-header').hide();
@@ -17,6 +18,8 @@ $(document).ready(function(){
         }else{
          $('.navigation').css('background-color', 'transparent');
         }
-        
+        if($(this).scrollTop() >= 300 ){
+            $('.about-word-area').show();
+          }
     });
 });
